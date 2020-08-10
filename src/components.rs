8ebb5c69c4eb8f69852258c3e1cb41830e131431
Alexander Baker <alex.diogenes@outlook.com>
2020-08-10 17:15:25 -0500

    Major refactoring; split out map, components, etc.

---
+++ src/components.rs
@@ -0,0 +1,22 @@
+use specs::prelude::*;
+use specs_derive::*;
+use rltk::{RGB};
+
+
+// STRUCTURES! Building blocks! Like classes, and can store data or attach functions to.
+// These "derive" macros tag components to save us lots of typing
+#[derive(Component)]
+pub struct Position {
+    pub x: i32,
+    pub y: i32,
+}
+
+#[derive(Component)]
+pub struct Renderable {
+    pub glyph: rltk::FontCharType,
+    pub fg: RGB,
+    pub bg: RGB,
+}
+
+#[derive(Component, Debug)]
+pub struct Player {}
