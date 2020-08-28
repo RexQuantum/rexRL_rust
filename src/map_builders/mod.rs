95e6af6ebec002643d44b9043c119cfae6af49d8
Alexander Baker <alex.diogenes@outlook.com>
2020-08-28 17:25:01 -0500

    interface module to extend map functionality

---
+++ src/map_builders/mod.rs
@@ -0,0 +1,11 @@
+/* 
+Modules are exposed through mod and pub mod; this file functions
+as an interface - specifically, a list of what is provided by the module,
+and how to interact with it. 
+*/
+
+use super::Map;
+
+trait MapBuilder {
+    fn build(new_depth: i32) -> Map;
+}
