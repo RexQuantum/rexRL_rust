5b59ffe24e863e0db01c79ee947696f5eefa86f6
RexQuantum <alex.diogenes@outlook.com>
2020-09-26 16:18:26 -0500

    I FINALLY HAVE A MAZE THAT WORKS

--- src/map_builders/maze.rs
+++ src/map_builders/maze.rs
@@ -58,28 +58,221 @@ impl MazeBuilder {
         }
     }
 
+    /* Maze code taken under MIT from https://github.com/cyucelen/mazeGenerator/ Good stuff,
+    there, even if it is a mess of c++ pointers */
+
     #[allow(clippy::map_entry)]
     fn build(&mut self) {
-        let mut rng = RandomNumberGenerator::new();        
+        let mut rng = RandomNumberGenerator::new();
 
-        // Find a starting point; start at the middle and walk left until we find an open tile
-        self.starting_position = Position{ x: self.map.width / 2, y : self.map.height / 2 };
-        let mut start_idx = self.map.xy_idx(self.starting_position.x, self.starting_position.y);
-        while self.map.tiles[start_idx] != TileType::Floor {
-            self.starting_position.x -= 1;
-            start_idx = self.map.xy_idx(self.starting_position.x, self.starting_position.y);
+       // Maze gen
+       let mut maze = Grid::new((self.map.width / 2)-2, (self.map.height / 2)-2, &mut rng);
+       maze.generate_maze(self);
+
+       // Find a starting point; start at the middle and walk left until we find an open tile
+       self.starting_position = Position{ x: 2, y : 2 };
+       let start_idx = self.map.xy_idx(self.starting_position.x, self.starting_position.y);
+       self.take_snapshot();
+
+       // Find all tiles we can reach from the starting point
+       let exit_tile = remove_unreachable_areas_returning_most_distant(&mut self.map, start_idx);
+       self.take_snapshot();
+
+       // Place the stairs
+       self.map.tiles[exit_tile] = TileType::DownStairs;
+       self.take_snapshot();
+
+       // Now we build a noise map for use in spawning entities later
+       self.noise_areas = generate_voronoi_spawn_regions(&self.map, &mut rng);
+    }
+}
+
+
+//Below We define four constants: TOP, RIGHT, BOTTOM and LEFT and assign them to the numbers 0..3.
+// We use these whenever the algorithm wants to refer to a direction. Looking at 
+// Cell, it is relatively simple: "row" and "column" define where the cell is on the map.
+//"walls" is an array, with a bool for each of the directions we've defined. 
+//Rust arrays (static, you can't resize them like a vector) are defined with 
+//the syntax [TYPE ; NUMBER_OF_ELEMENTS]. Most of the time we just use vectors
+//because we like the dynamic sizing; in this case, the number of elements is 
+//known ahead of time, so using the lower-overhead type makes sense.
+//"visited" - a bool indicating whether we've previously looked at the cell.
+
+
+const TOP : usize = 0;
+const RIGHT : usize = 1;
+const BOTTOM : usize = 2;
+const LEFT : usize = 3;
+
+#[derive(Copy, Clone)]
+struct Cell {
+    row: i32,
+    column: i32,
+    walls: [bool; 4],
+    visited: bool,
+}
+
+impl Cell {
+    fn new(row: i32, column: i32) -> Cell {
+        Cell{
+            row,
+            column,
+            walls: [true, true, true, true],
+            visited: false
         }
-        self.take_snapshot();
+    }
 
-        // Find all tiles we can reach from the starting point
-        let exit_tile = remove_unreachable_areas_returning_most_distant(&mut self.map, start_idx);
-        self.take_snapshot();
+    fn remove_walls(&mut self, next : &mut Cell) {
+        let x = self.column - next.column;
+        let y = self.row - next.row;
 
-        // Place the stairs
-        self.map.tiles[exit_tile] = TileType::DownStairs;
-        self.take_snapshot();
+        if x == 1 {
+            self.walls[LEFT] = false;
+            next.walls[RIGHT] = false;
+        }
+        else if x == -1 {
+            self.walls[RIGHT] = false;
+            next.walls[LEFT]= false;
+        }
+        if y == 1 {
+            self.walls[TOP] = false;
+            next.walls[BOTTOM] = false;
+        }
+        else if y == -1 {
+            self.walls[BOTTOM] = false;
+            next.walls[TOP]= false;
+        }
+    }
+}
 
-        // Now we build a noise map for use in spawning entities later
-        self.noise_areas = generate_voronoi_spawn_regions(&self.map, &mut rng);
+struct Grid<'a> {
+    width: i32,
+    height: i32,
+    cells: Vec<Cell>,
+    backtrace: Vec<usize>,
+    current: usize,
+    rng : &'a mut RandomNumberGenerator
+}
+
+impl<'a> Grid<'a> {
+    fn new(width: i32, height: i32, rng: &mut RandomNumberGenerator) -> Grid {
+        let mut grid = Grid{
+            width,
+            height,
+            cells: Vec::new(),
+            backtrace: Vec::new(), 
+            current: 0,
+            rng
+        };
+
+        for row in 0..height {
+            for column in 0..width {
+                grid.cells.push(Cell::new(row, column));
+            }
+        }
+
+        grid
+    }
+
+    fn calculate_index(&self, row: i32, column: i32) -> i32 {
+        if row < 0 || column < 0 || column > self.width-1 || row > self.height-1 {
+            -1
+        } else {
+            column + (row * self.width)
+        }
+    }
+
+    fn get_available_neighbors(&self) -> Vec<usize> {
+        let mut neighbors : Vec<usize> = Vec::new();
+
+        let current_row = self.cells[self.current].row;
+        let current_column = self.cells[self.current].column;
+
+        let neighbor_indices : [i32; 4] = [
+            self.calculate_index(current_row - 1, current_column),
+            self.calculate_index(current_row, current_column + 1),
+            self.calculate_index(current_row + 1, current_column),
+            self.calculate_index(current_row, current_column - 1)
+        ];
+
+        for i in neighbor_indices.iter() {
+            if *i != -1 && !self.cells[*i as usize].visited {
+                neighbors.push(*i as usize);
+            }
+        }
+
+        neighbors
+    }
+
+    fn find_next_cell(&mut self) -> Option<usize> {
+        let neighbors = self.get_available_neighbors();
+        if !neighbors.is_empty() {
+            if neighbors.len() == 1 {
+                return Some(neighbors[0]);
+            } else {
+                return Some(neighbors[(self.rng.roll_dice(1, neighbors.len() as i32)-1) as usize]);
+            }
+        }
+        None
+    }
+
+    fn generate_maze(&mut self, generator : &mut MazeBuilder) {
+        let mut i = 0;
+        loop {
+            self.cells[self.current].visited = true;
+            let next = self.find_next_cell();
+
+            match next {
+                Some(next) => {
+                    self.cells[next].visited = true;
+                    self.backtrace.push(self.current);
+                    /*
+
+                    vvv higher_part vvv
+                    |#######cell2######|
+                    |#######cell1######|
+                     ^^^ lower_part ^^^
+
+                     */
+                     let (lower_part, higher_part) =
+                     self.cells.split_at_mut(std::cmp::max(self.current, next));
+                 let cell1 = &mut lower_part[std::cmp::min(self.current, next)];
+                 let cell2 = &mut higher_part[0];
+                 cell1.remove_walls(cell2);
+                 self.current = next;
+             }
+             None => {
+                 if !self.backtrace.is_empty() {
+                     self.current = self.backtrace[0];
+                     self.backtrace.remove(0);
+                 } else {
+                     break;
+                 }
+             }
+         }
+
+         if i % 50 == 0 {
+             self.copy_to_map(&mut generator.map);
+             generator.take_snapshot();
+         }
+         i += 1;
+     }
+ }
+
+    fn copy_to_map(&self, map : &mut Map) {
+        // clear the map
+        for i in map.tiles.iter_mut() { *i = TileType::Wall; }
+
+        for cell in self.cells.iter() {
+            let x = cell.column +1;
+            let y = cell.row +1;
+            let idx = map.xy_idx(x * 2, y * 2);
+
+            map.tiles[idx] = TileType::Floor;
+            if !cell.walls[TOP] { map.tiles[idx - map.width as usize] = TileType::Floor }
+            if !cell.walls[RIGHT] { map.tiles[idx + 1] = TileType::Floor }
+            if !cell.walls[BOTTOM] { map.tiles[idx + map.width as usize] = TileType::Floor }
+            if !cell.walls[LEFT] { map.tiles[idx - 1] = TileType::Floor }
+        }
     }
 }
