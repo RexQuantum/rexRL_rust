f0643ad3a5877f3679200500a3c0c06c52b0d4e1
RexQuantum <alex.diogenes@outlook.com>
2020-09-26 19:56:22 -0500

    fixed map selector randomization

--- src/map_builders/mod.rs
+++ src/map_builders/mod.rs
@@ -34,8 +34,8 @@ pub fn random_builder(new_depth: i32) -> Box<dyn MapBuilder> {
         4 => Box::new(DrunkardsWalkBuilder::open_area(new_depth)),
         5 => Box::new(DrunkardsWalkBuilder::open_halls(new_depth)),
         6 => Box::new(DrunkardsWalkBuilder::winding_passages(new_depth)),
-        7 => Box::new(MazeBuilder::new(new_depth)),
-        _ => Box::new(SimpleMapBuilder::new(new_depth))
+        7 => Box::new(SimpleMapBuilder::new(new_depth)),
+        _ => Box::new(MazeBuilder::new(new_depth)),
     }
     
 }
