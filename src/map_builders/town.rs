ba8de35f78250db4b200213ae621fee68cd449ae
Rex Baker <alex.diogenes@outlook.com>
2021-02-23 05:41:27 -0600

    workin' on getting town map builder set up

--- src/map_builders/town.rs
+++ src/map_builders/town.rs
@@ -1,4 +1,6 @@
-use super::BuilderChain;
+use super::{BuilderChain, BuilderMap, InitialMapBuilder, TileType, Position};
+use std::collections::HashSet;
+
 
 pub fn level_builder(new_depth: i32, rng: &mut rltk::RandomNumberGenerator, width: i32, height: i32) -> BuilderChain {
     let mut chain = BuilderChain::new(new_depth, width, height);
@@ -24,9 +26,23 @@ impl TownBuilder {
     }
 
     pub fn build_rooms(&mut self, rng: &mut rltk::RandomNumberGenerator, build_data : &mut BuilderMap) {
+        self.grass_layer(build_data);
+        self.water_and_piers(rng, build_data);
+        
+        // Make visible for procgen snapshot/screenshot
+        for t in build_data.map.visible_tiles.iter_mut() {
+            *t = true;
+        }
+        build_data.take_snapshot();
     }
-}
 
-pub fn build_rooms(&mut self, rng: &mut rltk::RandomNumberGenerator, build_data : &mut BuilderMap) {
+    fn grass_layer(&mut self, build_data : &mut BuilderMap) {
+        // Let's lay down some soft organic matter
+        for t in build_data.map.tiles.iter_mut() {
+            *t = TileType::Grass;
+        }
+        build_data.take_snapshot();
+    }
+
     
 }
