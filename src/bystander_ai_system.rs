8c8caabf4152d1ec6ccbcda42f5b99685dc383b5
Alexander Diogenes <alex.diogenes@outlook.com>
2021-08-09 16:13:19 -0500

    .

--- src/bystander_ai_system.rs
+++ src/bystander_ai_system.rs
@@ -5,5 +5,21 @@ pub struct BystanderAI {}
 
 impl <'a> System<'a> for BystanderAI {
     #[allow(clippy::type_complexity)]
-    
+    type SystemData = ( WriteExpect<'a, Map>,
+                        ReadExpect<'a, RunState>,
+                        Entities<'a>,
+                        WriteStorage<'a, Viewshed>,
+                        ReadStorage<'a, Bystander>,
+                        WriteStorage<'a, Position>,
+                        WriteStorage<'a, EntityMoved>,
+                        WriteExpect<'a, rltk::RandomNumberGenerator>);
+
+    fn run(&mut self, data : Self::SystemData) {
+        let (mut map, runstate, entities, mut viewshed, bystander, mut position,
+            mut entity_moved, mut rng) = data;
+
+        if *runstate != RunState::MonsterTurn { return; }
+    }
+
 }
+
