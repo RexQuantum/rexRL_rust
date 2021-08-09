3d1f230097e0468bd45b802b92f074625752854c
Alexander Diogenes <alex.diogenes@outlook.com>
2021-08-09 16:08:19 -0500

    bystander AI roaming

---
+++ src/bystander_ai_system.rs
@@ -0,0 +1,9 @@
+use specs::prelude::*;
+use super::{Viewshed, Bystander, Map, Position, RunState, EntityMoved};
+
+pub struct BystanderAI {}
+
+impl <'a> System<'a> for BystanderAI {
+    #[allow(clippy::type_complexity)]
+    
+}
